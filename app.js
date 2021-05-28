5c5f22422142ea40791102d8752b303c62236418
Erland <erland.vo@telenet.be>
2021-05-28 23:52:27 +0200

    first working version. todo: about page

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
 const express = require('express');
-const data = require('./data.json');
-
 
 const app = express();
 
-
 app.use(express.json());                         // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
-//app.use(cookieParser());
 
-// tell express that static content sits inside the 'public' folder.
-// This means the contents of the public folder is now available as root 
-// route. I.e. the file public/css/styles.css is available as 
-// http://domain.tld/css/styles.css. If there is a 'css' route and this
-// causes a conflict, you can remap this to a custom route (e.g. 'static')
-// as shown below. The '/static' parameter is optional. 
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
+
+const data = require('./data.json');
 
 app.set('view engine', 'pug');
-//app.set('views', './views');    // 'views' is the default subfolder so this is optional
 
 // include routes from the 'routes' directory. 
 // index.js is the default name and thus './routes' would suffice.
 // const mainRoutes = require('./routes/index.js'); 
 
-app.get('/', (req,res) => { // render "Home"
-    console.log(data.projects);
-    res.send('Hello world');
+app.get('/', (req,res) => { 
+    res.render('index.pug', { projects: data.projects });
 });
 
-app.get('/about', (req,res) => { // render "About"
-    res.send('Hello world');
+app.get('/about', (req,res) => { 
+    res.render('about.pug');
 });
 
+// the below route does not handle '/project/' !
+app.get('/project/:id', (req, res) => {
+    // the id's are 1-based, but the array is 0-based
+    const index = req.params.id - 1;
+    //console.log(`data[${index}]`, data.projects[index]);
+    res.render('project.pug', { project: data.projects[index] });
+});
 
+// Todo: handle 404 errors
+// The 404 handler should create a custom new Error(), set its status property 
+// to 404 and set its message property to a user friendly message. Then the 404 
+// handler should log out the new error's message and status properties.
+
+// Todo: handle server errors
+// After the 404 handler in app.js add a global error handler that will deal 
+// with any server errors the app encounters. This handler should ensure that 
+// there is an err.status property and an err.message property if they don't 
+// already exist, and then log out the err object's message and status.
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
