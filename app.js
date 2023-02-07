ee9c0867058ecc5f26222a5e7361663f64837488
Erland <erland.vo@telenet.be>
2023-02-07 16:39:30 +0100

    added components section

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ app.set('views', path.join(__dirname, 'views')); // added
 
 // import data:
 const { projects } = require('./data.json');
+const { components } = require('./components.json');
 
 // handle routes:
 app.get('/', (req, res) => { 
-    res.render('index.pug', { projects });
+    res.render('index.pug', { projects, components });
 });
 
 app.get('/about', (req, res) => { 
@@ -36,6 +37,29 @@ app.get('/project/:id', (req, res, next) => {
         for (let i = 0; i < projects.length; i++) {
             if (projects[i].id === index) {
                 res.render('project.pug', { project: projects[i] });
+                return;
+            } 
+        }
+        next(); // project not found, give the user a 404
+    }    
+});
+
+app.get('/component/:id', (req, res, next) => {
+    // the id's are 1-based, but the array is 0-based 
+    const index = +req.params.id;// - 1;
+
+    // If the 'id' string provided after '/project/' is not a number 
+    // or is too big or too small a number, make it a 404 error
+    if((typeof index != 'number') || isNaN(index) || 
+        index < 0) {
+        // we can't handle this route here (it does not exist) so we go to 
+        // the next middleware in the chain, which will render a 404 error
+        next();
+    } else {
+        // check if the index is an existing one and dig up the right component:
+        for (let i = 0; i < components.length; i++) {
+            if (components[i].id === index) {
+                res.render('component.pug', { component: components[i] });
                 return;
             } 
         }
